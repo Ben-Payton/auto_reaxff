aff1f20effd404334fe41941e90e1b8475317272
Ben Payton <bennyp2494@gmail.com>
2025-07-15 10:33:58 -0600

    initial commit

---
+++ utils/xyz_to_lammps.py
@@ -0,0 +1,7 @@
+##########################################################################################
+"""
+This file is meant to hold tools that will take an xyz_molecule file and turn it in to a
+base input molecule file for lammps
+"""
+##########################################################################################
+
