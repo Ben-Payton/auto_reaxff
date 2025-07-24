ff5734c2d014afdf53e2b2d30d03a0d0e4638a10
Ben Payton <bennyp2494@gmail.com>
2025-07-24 16:36:08 -0600

    can extract needed data from tpr and trr files and make atoms and velocities section of data files

--- docs/steps_to_run_reaxff.md
+++ docs/steps_to_run_reaxff.md
@@ -5,12 +5,22 @@ Initial inputs to solution phase lammps files involves putting some number of mo
 - Initial goal is to be able to feed in a base xyz file in to this project, turn it in to a molecule and specify how many of them I would like in the box
 - Then I would like to solvate it in tip3p water.
 
+This will be acheived by using the typical equilibration work that is implemented then bringing the simulation down to near 0K.
+The .cpt file will be useful as it is a restart file.
+1. use a .ctp file as an input file
+2. ramp the temperature down using NVT ensemble
+3. output .trr file with positions and velocities
+
+
 # Running Initial Equilibration Runs
 
 - NVP ensemble
 - NVT ensemble
 - Check system equilibration
 - Bring tmeperature down to near 0K
+
+-  final output files need coords and velocities
+    trr files have coordinates tpr files have both, but cannot be read by MDAnalysis 
 
 # Switch to ReaxFF forcefield
 
--- utils/xyz_to_lammps.py
+++ utils/xyz_to_lammps.py
@@ -4,4 +4,60 @@ This file is meant to hold tools that will take an xyz_molecule file and turn it
 base input molecule file for lammps
 """
 ##########################################################################################
+import MDAnalysis as mda
+import os
+import numpy as np
 
+
+
+def check_trr(TPR_PATH,TRR_PATH):
+    u = mda.Universe(TPR_PATH,TRR_PATH)
+    positions = u.atoms.positions
+    velocities = u.atoms.velocities
+    charges = u.atoms.charges
+    atom_labels = u.atoms.types
+    molecule_ids = [atom.residue.resid for atom in u.atoms]
+
+    data_dict = {
+        "positions":positions,
+        "velocities":velocities,
+        "charges":charges,
+        "atom_type":atom_labels,
+        "molecule_ids":molecule_ids,
+    }
+    return data_dict
+
+def make_velocity_section(data_dict):
+    string_to_format = "{atom_id} {vx} {vy} {vz}\n"
+    final_string = "Velocities\n\n"
+    for index, velocity in enumerate(data_dict["velocities"]):
+        final_string = final_string + string_to_format.format(atom_id=index+1,vx=velocity[0],vy=velocity[1],vz=velocity[2])
+    return final_string
+
+def make_atoms_section(data_dict):
+    string_to_format = "{atom_id} {molecule_id} {atom_type} {charge} {x_pos} {y_pos} {z_pos}\n"
+    final_string = "Atoms\n\n"
+    for index, postion in enumerate(data_dict["positions"]):
+        final_string = final_string + string_to_format.format(atom_id=index+1,
+                                                              molecule_id=data_dict["molecule_ids"][index],
+                                                              atom_type=data_dict["atom_type"][index],
+                                                              charge=data_dict["charges"][index],
+                                                              x_pos=postion[0],
+                                                              y_pos=postion[1],
+                                                              z_pos=postion[2],)
+    return final_string
+
+
+def main():
+
+
+    # HARD CODED PATHS TO FILES DO NOT FORGET TO CHANGE LATER
+    TPR = "/home/ben/01_Projects/work_projects/auto_reaxff/test_files/gromacs_scripts_testing/salt_water-npt-prod.tpr"
+    TRR = "/home/ben/01_Projects/work_projects/auto_reaxff/test_files/gromacs_scripts_testing/salt_water-npt-prod.part0001.trr"
+
+    data_dict = check_trr(TPR,TRR)
+
+
+
+if __name__ == "__main__":
+    main()
